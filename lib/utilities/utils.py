a8e34a0adfc581d6bd064fa83ed365a3209d74b2
Ezequiel Salas <salaseze@gmail.com>
2022-06-20 16:35:42 -0300

    read_file_to_array utils

--- lib/utilities/utils.py
+++ lib/utilities/utils.py
@@ -1,5 +1,8 @@
+from numpy import loadtxt
+
+
 def write_array_to_file(array, text_file_name):
-    outFile = open(text_file_name, "wb")
+    outFile = open(text_file_name, "w")
     size = len(array)
     counter = 0
     while counter < size:
@@ -9,3 +12,10 @@ def write_array_to_file(array, text_file_name):
         counter = counter + 1
 
     print(f"Successfully stored {size} random numbers in file named: '{text_file_name}'")
+
+    outFile.close()
+
+
+def read_file_to_array(text_file_name):
+    array = loadtxt(text_file_name)
+    return array
